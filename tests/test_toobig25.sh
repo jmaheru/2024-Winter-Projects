7abf8d04da26bbcbc9a52c50c93bff159972af61
jmaheru <156146067+jmaheru@users.noreply.github.com>
2024-12-25 21:17:11 -0800

    Add files via upload

---
+++ tests/test_toobig25.sh
@@ -0,0 +1,29 @@
+# Creates the example output
+echo "TOO BIG" > expected.txt
+
+# Runs the program
+./calc 512 513 > output.txt 
+
+# Ensures exit code is non-Zero
+if [ $? -eq 0 ]; then
+    echo "invalid exit code" $?
+    rm output.txt
+    rm expected.txt
+    exit 1
+fi
+
+#Compare bad_input with output
+diff output.txt expected.txt
+if [ $? -eq 1 ]; then
+    echo "Test failed expected."
+    rm output.txt
+    rm expected.txt
+    exit 1
+fi
+
+# Prints a message on success
+echo "Output was TOO BIG: PASS."
+# Cleans up files created
+rm output.txt
+rm expected.txt
+exit 0
